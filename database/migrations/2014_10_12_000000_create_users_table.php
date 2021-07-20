e47efa50291cc3105a98be582905955a1d78efe2
Nathi Taengnim <nathi.t@outlook.com>
2021-07-20 19:31:23 +0100

    update auth views

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->default('');
+            $table->tinyInteger('enabled')->nullable(false)->default(1);
+            $table->tinyInteger('is_admin');
+            $table->unsignedInteger('purchased_credits')->nullable(false)->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
